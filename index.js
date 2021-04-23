587961f3c6cd0d5c10c4b13736aa8ca8944cc201
SethWorkPeepsNet <sguimont@peoplenetonline.com>
2021-04-23 11:57:41 -0500

    gitignore fix

--- index.js
+++ index.js
@@ -10,10 +10,13 @@ newman.run({
     collection: 'TranmitService.postman_collection.json',
     reporters:  'html',
         reporter:{
-            html: {export: 'results.html'
+            html: {
+                export: 'results.html'
             }
         }
 },function(err){
     if (err){throw err;}
     console.log('collecton run complete');
 });
+
+
